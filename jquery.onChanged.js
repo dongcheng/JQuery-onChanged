72a8fbf27c47eb55933fb69fceef4d62a738a9ea
Yes2Me <gccinls@gmail.com>
2012-03-03 03:28:29 +0800

    edit project url

--- jquery.onChanged.js
+++ jquery.onChanged.js
@@ -1,6 +1,6 @@
 /**
  * onChanged
- * Project page - 
+ * Project page - http://yes2.me/onchanged-project
  * Copyright (c) 2008 Andris Valums, http://yes2.me
  * Licensed under the MIT license (http://en.wikipedia.org/wiki/MIT_License)
  * Version 0.1 (3.3.2011)
