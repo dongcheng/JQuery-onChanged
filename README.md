690ebf883fae37f02e4360837a13b937ebf67191
Yes2Me <gccinls@gmail.com>
2012-03-04 14:51:13 +0800

    update to ver 0.2

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
-====jQuery - onChanged
+jQuery - onChanged Plugin  v0.2
+=========
+*  用于实时检测 `<input>` `<textarea>` 元素内容变化的事件。
+*  项目地址: [http://yes2.me/onchanged-project](http://yes2.me/onchanged-project "JQuery-onChanged")
 
-用于检测 input textarea 元素内容变化的事件。
-项目地址: http://yes2.me/onchanged-project
-===使用方法
+为什么使用这个插件
+---------
+*  Javascript `onchange` 方法：只有在`<input>` `<textarea>` 失去焦点才执行回调函数，无法实时检测。
+*  使用 `.keyup()`  `.keydown()` 判断`value`的变化，对鼠标操作无效。
+*  使用 `jQuery.onChanged` 可以解决上述问题。 
 
-  - 绑定内容检测事件
-	$("textarea").focus(function() {
-		$(this).onChanged(function(){
-			//处理事件
-		}); 
+使用方法
+---------
+
+	$("textarea").onChanged(function() {
+		//处理事件
 	});
-	      
-  - 解除内容检测事件
-	$("textarea").blur(function() {
-		$(this).stopOnChanged();
-	});
+	
+更新日志
+---------
+*  v0.2   2011.03.04
+
+	[更新]：去除stopOnChanged函数，使用更加简单。
+
+*  v0.1   2011.03.03 ：
+	
+	初始版本建立
--- jquery.onChanged.js
+++ jquery.onChanged.js
@@ -3,31 +3,38 @@
  * Project page - http://yes2.me/onchanged-project
  * Copyright (c) 2012 Yes2Me, http://yes2.me
  * Licensed under the MIT license (http://en.wikipedia.org/wiki/MIT_License)
- * Version 0.1 (3.3.2012)
+ * Version 0.2 (4.3.2012)
  */
 
 (function($) {
+	var prevtext = "";
 	function onChangedstart(obj, callback) {
 		if (obj.val() == null || obj.val() == "") {
 			return;
 		} 
-		if (obj.attr("prevtext") != obj.val()) {
-			obj.attr("prevtext", obj.val());
-			callback();
+		if (prevtext != obj.val()) {
+			prevtext = obj.val();
+			if (callback != undefined)
+				callback();
 		}
 	} 
 
 	$.fn.extend({
 		onChanged: function(callback) {
+			var id;
 			$this = $(this);
-			id = window.setInterval(function(){
-				onChangedstart($this, callback);
-			}, 300);
-			$this.attr("onChangedId", id);
-		},
-		stopOnChanged: function() {
-			window.clearInterval($(this).attr("onChangedId"));
-			$(this).attr("prevtext", "");
+
+			$this.focus(function() {
+				id = window.setInterval(function(){
+					onChangedstart($this, callback);
+				}, 300);
+			});
+
+			$this.blur(function() {
+				onChangedstart($this);
+				window.clearInterval(id);
+				prevtext = "";
+			});
 		}
 	});
 })(jQuery);
